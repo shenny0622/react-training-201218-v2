1205549845192840e0ab86c2a33669816e39efeb
shenny0622 <65540548+shenny0622@users.noreply.github.com>
2020-12-30 02:07:12 +0800

    update

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,62 +5,73 @@ import phone from '../images/icons_phone.png'
 import tag from '../images/icons_tag.png'
 
 
-const Card = () => {
-    const [state, setState] = useState({
-      // items: [],
-        image: '',
-        name: '',
-        zone: '',
-        opentime:'',
-        address:'',
-        tel:'',
-        info:'',
-    
-    });
+const Card = (props) => {
+    // const [state, setState] = useState(
+    //   {Picture1:'',
+    //     Name   :'',
+    //     Zone   :'',
+    //     Opentime:'',
+    //     Add:'',
+    //     Tel : '',
+    //     Ticketinfo:''}
+    // );
+    // props:{
+    //   item:{
+    //     Picture1:
+    //     Name:''
+    //     Zone:''
+    //     Opentime:''
+    //     Add:''
+    //     Tel: ''
+    //     Ticketinfo:''
+    //   }
+    // }
 
 //api
-    useEffect(()=>{
-        fetch( 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json',{method:"GET"})
-          .then(res => res.json())
-          .then(
-            (data) => {                
-              const card =  data.result.records[1];
+  //   useEffect(()=>{
+  //       fetch( 'https://raw.githubusercontent.com/hexschool/KCGTravel/master/datastore_search.json',{method:"GET"})
+  //         .then(res => res.json())
+  //         .then(
+  //           (data) => {                
+  //             const card =  data.result.records[1];
               
-              setState({
-                // items: data.result.records
-                image: card.Picture1,
-                name: card.Name,
-                zone: card.Zone,  
-                opentime: card.Opentime,
-                address: card.Add,
-                tel: card.Tel,
-                info: card.Ticketinfo,   
-              });
-            });
-            // (sError) => {
-            //   setState({
-            //     isLoaded: true,
-            //     error:sError,
-            //     items:null
-            //   });
-            // }   
-  },[]);
+  //             setState({
+  //               // items: data.result.records
+  //               image: card.Picture1,
+  //               name: card.Name,
+  //               zone: card.Zone,  
+  //               opentime: card.Opentime,
+  //               address: card.Add,
+  //               tel: card.Tel,
+  //               info: card.Ticketinfo,   
+  //             });
+  //           });
+  //           // (sError) => {
+  //           //   setState({
+  //           //     isLoaded: true,
+  //           //     error:sError,
+  //           //     items:null
+  //           //   });
+  //           // }   
+  // },[]);
 
-  const { image, name, zone,opentime,address,tel,info } = state;
+  // const { image, name, zone,opentime,address,tel,info } = state;
+  // const {Picture1,Name,Zone,Opentime,Add,Tel,Ticketinfo} = props;
+  const {item} =  props;
   return(
     <li className="list-card">
-      <div className="img" style={{backgroundImage: `url(${image})`}}>
+      <div className="img" style={{backgroundImage: `url(${item.Picture1})`}}>
           <div class="img-title">
-              <h3 className="title-24px">{name}</h3>
-              <p className="title-16px">{zone}</p>
+              <h3 className="title-24px">{item.Name}</h3>
+              <p className="title-16px">{item.Zone}</p>
           </div>
       </div>
       <div className="content-card">
-          <p><img src={clock}  alt="icon"/>{opentime}</p>
-          <p><img src={pin}  alt="icon"/>{address}</p>
+          <p><img src={clock}  alt="icon"/>{item.Opentime}</p>
+          <p><img src={pin}  alt="icon"/>{item.Add}</p>
           <div className="card_down_area">
-            <p><img src={phone}  alt="icon"/>{tel}</p>
-            <p><img src={tag}  alt="icon"/>{info}</p>
+            <p><img src={phone}  alt="icon"/>{item.Tel}</p>
+            <p><img src={tag}  alt="icon"/>{item.Ticketinfo}</p>
           </div>
       </div>
       {/* <img src={image}  alt=""/> */}
--- src/components/CardsContent.js
+++ src/components/CardsContent.js
@@ -1,5 +1,6 @@
 // import React from 'react';
 import React,{useState, useEffect} from "react";
+import Card from './Card';
 
 
 
@@ -45,9 +46,14 @@ const CardsContent = () =>{
     return (
       <div className="main">
           <h2 className="title-main">請先選擇區域</h2>
-          {cards.map((card) =>(
-            <ul className="list">{card.Name}</ul>
+       
+        {/* 下方要測試撈出五筆資料，不要撈全部 */}
+          {cards.map((card,index) =>(
+            <ul className="list"><Card item={card}/></ul>
           ))} 
+          {/* itemZones: data.result.records.map((item)=>(item.Zone)).filter(function(element, index, arr){
+                    return arr.indexOf(element) === index;
+                }) */}
       </div>
     );
   }
